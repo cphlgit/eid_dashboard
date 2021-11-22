13dba0c76df22e5227449ee8a6d465f5935f177d
ponita <poniagusto@gmail.com>
2021-11-22 20:02:32 +0300

    addition of eight weeks' view

--- resources/views/sections/_poc_sites_statistics.blade.php
+++ resources/views/sections/_poc_sites_statistics.blade.php
@@ -2,7 +2,9 @@
     
 
     <div class="col-lg-12 facilties-sect " >
-    
+    <?php 
+    $currentWeekNumber = date('W');
+     ?>
         <div>
             <span style="font-size: 10px; color: #F44336;">
             </span>
@@ -20,6 +22,15 @@
                                     <th>District</th>
                                     <th>Device</th>
                                     <th>#Tests</th>
+                                    <th># $currentWeekNumber</th>
+                                    <th>#Last week</th>
+                                    <th>#Week2</th>
+                                    <th>#Week3</th>
+                                    <th>#Week4</th>
+                                    <th>#Week5</th>
+                                    <th>#Week6</th>
+                                    <th>#Week7</th>
+                                    <th>#Week8</th>
                                     <th>#Negatives</th>
                                     <th>#Positives</th>
                                     <th>Positivity Rate</th>
@@ -36,6 +47,15 @@
                                         <td class="ng-cloak"><% f.district %></td>
                                         <td class="ng-cloak"><% f.poc_device %></td>
                                         <td class="ng-cloak"><% f.tests %></td>
+                                        <td class="ng-cloak"><% f.thiswk %></td>
+                                        <td class="ng-cloak"><% f.wk1 %></td>
+                                        <td class="ng-cloak"><% f.wk2 %></td>
+                                        <td class="ng-cloak"><% f.wk3 %></td>
+                                        <td class="ng-cloak"><% f.wk4 %></td>
+                                        <td class="ng-cloak"><% f.wk5 %></td>
+                                        <td class="ng-cloak"><% f.wk6 %></td>
+                                        <td class="ng-cloak"><% f.wk7 %></td>
+                                        <td class="ng-cloak"><% f.wk8 %></td>
                                         <td class="ng-cloak"><% f.negatives %></td>
                                         <td class="ng-cloak"><% f.positives %></td>
                                         <td class="ng-cloak"><% ((f.positives/(f.negatives + f.positives))*100)| number:1 %> %</td>

d6b2417939435fbd1e86484948ead4e7f1a2cab6
ponita <poniagusto@gmail.com>
2021-11-23 17:54:29 +0300

    updated poc data view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1238,9 +1238,7 @@ db.eid_dashboard.aggregate(
 	}
 
 	public function getPocFacilityStatistics(){
-			$from = $request->get('start');
-        	$to = $request->get('end');
-			$sql = "SELECT district, facility, COUNT(DISTINCT facility) AS peripheral_sites, poc_device, SUM(CASE WHEN p.created_at > DATE_SUB(NOW(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as thiswk,
+			$sql = "SELECT district, f.id, facility, COUNT(DISTINCT facility) AS peripheral_sites, poc_device, SUM(CASE WHEN p.created_at > DATE_SUB(NOW(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as thiswk,
  SUM(CASE WHEN p.created_at BETWEEN DATE_SUB(NOW(), INTERVAL 1 WEEK) AND NOW() THEN 1 ELSE 0 END) as wk1, SUM(CASE WHEN p.created_at BETWEEN DATE_SUB(NOW(), INTERVAL 2 WEEK) AND DATE_SUB(NOW(), INTERVAL 1 WEEK) THEN 1 ELSE 0 END) as wk2, SUM(CASE WHEN p.created_at BETWEEN DATE_SUB(NOW(), INTERVAL 3 WEEK) AND DATE_SUB(NOW(), INTERVAL 2 WEEK) THEN 1 ELSE 0 END) as wk3, SUM(CASE WHEN p.created_at BETWEEN DATE_SUB(NOW(), INTERVAL 4 WEEK) AND DATE_SUB(NOW(), INTERVAL 3 WEEK) THEN 1 ELSE 0 END) as wk4, SUM(CASE WHEN p.created_at BETWEEN DATE_SUB(NOW(), INTERVAL 5 WEEK) AND DATE_SUB(NOW(), INTERVAL 4 WEEK) THEN 1 ELSE 0 END) as wk5, SUM(CASE WHEN p.created_at BETWEEN DATE_SUB(NOW(), INTERVAL 6 WEEK) AND DATE_SUB(NOW(), INTERVAL 5 WEEK) THEN 1 ELSE 0 END) as wk6, SUM(CASE WHEN p.created_at BETWEEN DATE_SUB(NOW(), INTERVAL 7 WEEK) AND DATE_SUB(NOW(), INTERVAL 6 WEEK) THEN 1 ELSE 0 END) as wk7, SUM(CASE WHEN p.created_at BETWEEN DATE_SUB(NOW(), INTERVAL 51 WEEK) AND DATE_SUB(NOW(), INTERVAL 52 WEEK) THEN 1 ELSE 0 END) as wk8, max(created_at) AS latest_date,
 				COUNT(p.id) AS tests,  
 				SUM(CASE WHEN results='Negative' THEN 1 ELSE 0 END) AS negatives,
